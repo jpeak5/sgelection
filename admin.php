873dde95b5dab0de9aea5a3d2fc2bad400c1fd8e
Jason Peak <jpeak5@lsu.edu>
2014-08-01 08:57:01 -0500

    update url construction to reduce repeated typing

--- admin.php
+++ admin.php
@@ -5,18 +5,18 @@ require_once('admin_form.php');
 
 global $DB, $OUTPUT, $PAGE;
 
-//next look for optional variables.
-$done = optional_param('done', 0, PARAM_TEXT);
+$done    = optional_param('done', 0, PARAM_TEXT);
+$selfurl = '/blocks/sgelection/admin.php';
 
 $PAGE->set_context(context_system::instance());
-$PAGE->set_url('/blocks/sgelection/admin.php');
+$PAGE->set_url($selfurl);
 $PAGE->set_pagelayout('standard');
 $PAGE->set_heading(get_string('admin_page_header', 'block_sgelection'));
 
 require_login();
 
 $settingsnode = $PAGE->settingsnav->add(get_string('sgelectionsettings', 'block_sgelection'));
-$editurl = new moodle_url('/blocks/sgelection/admin.php');
+$editurl = new moodle_url($selfurl);
 $editnode = $settingsnode->add(get_string('editpage', 'block_sgelection'), $editurl);
 $editnode->make_active();
 
@@ -29,10 +29,9 @@ if($form->is_cancelled()){
     set_config('commissioner', $fromform->commissioner, 'block_sgelection');
     set_config('fulltime', $fromform->fulltime, 'block_sgelection');
     set_config('parttime', $fromform->parttime, 'block_sgelection');
-
     set_config('excluded_curr_codes', implode(',', $fromform->excluded_curr_codes), 'block_sgelection');
 
-    redirect(new moodle_url('/blocks/sgelection/admin.php?done=true'));
+    redirect(new moodle_url($selfurl, array('done'=>'true')));
 } else {
     // form didn't validate or this is the first display
     $site = get_site();
