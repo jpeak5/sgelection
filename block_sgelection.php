b5c76ab1521abf579b3a110e1fb1bb44fd53b0f7
Jason Peak <jpeak5@lsu.edu>
2014-08-04 17:21:44 -0500

    yet another place where the election refactor has effect

--- block_sgelection.php
+++ block_sgelection.php
@@ -1,5 +1,6 @@
 <?php
 require_once($CFG->dirroot.'/blocks/sgelection/lib.php');
+require_once 'lib.php';
 
 class block_sgelection extends block_list {
 
@@ -9,7 +10,7 @@ class block_sgelection extends block_list {
 
     public function get_content() {
         global $USER, $CFG, $COURSE, $OUTPUT;
-        
+
         $this->content = new stdClass;
         $this->content->items = array();
         $this->content->icons = array();
@@ -20,7 +21,8 @@ class block_sgelection extends block_list {
         $activeElectionsLinks = array();
         $i=0;
         foreach($activeElections as $ae){
-            $activeElectionsLinks[] = html_writer::link( new moodle_url('/blocks/sgelection/ballot.php', array('election_id' => $ae->id)), 'Ballot for ' . $ae->year . ' ' . $ae->sem_code );
+            $semester = sge::get_semester_name($ae->semester);
+            $activeElectionsLinks[] = html_writer::link( new moodle_url('/blocks/sgelection/ballot.php', array('election_id' => $ae->id)), 'Ballot for ' . $semester );
             $this->content->items[]= $activeElectionsLinks[$i];
             $this->content->icons[] = $OUTPUT->pix_icon('t/edit', 'admin', 'moodle', $icon_class);
             $i++;
@@ -31,16 +33,16 @@ class block_sgelection extends block_list {
         $commissioner = html_writer::link(new moodle_url('/blocks/sgelection/commissioner.php'), 'Commissioner');
         $this->content->items[] = $vote;
         $this->content->icons[] = $OUTPUT->pix_icon('t/check', 'vote', 'moodle', $icon_class);
-        
+
         $this->content->items[] = $administrate;
         $this->content->icons[] = $OUTPUT->pix_icon('t/edit', 'admin', 'moodle', $icon_class);
-        
+
         $this->content->items[] = $commissioner;
         $this->content->icons[] = $OUTPUT->pix_icon('t/edit', 'admin', 'moodle', $icon_class);
 
         return $this->content;
     }
-    
+
     public function instance_allow_multiple() {
         return false;
     }
