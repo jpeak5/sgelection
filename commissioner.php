9d288bc734e8bee4abff16eb183ac7cc12e185a3
Jason Peak <jpeak5@lsu.edu>
2014-08-05 11:36:13 -0500

    some cleanup

--- commissioner.php
+++ commissioner.php
@@ -36,12 +36,9 @@ if($form->is_cancelled()){
     $url = new moodle_url('/');
     redirect($url);
 } else if($fromform = $form->get_data()){
-    $unixStartTime = $fromform->start_date;
-    $unixEndTime = $fromform->end_date;
     $electionData = new election($fromform);
     $electionData->save();
-    $url = new moodle_url('ballot.php', array('election_id' => $electionData->id));
-    redirect($url);
+    redirect(new moodle_url('ballot.php', array('election_id' => $electionData->id)));
 } else {
     $site = get_site();
     echo $OUTPUT->header();
