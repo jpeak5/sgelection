a6a7641d5bbea58538ec05ce4bffc409c9df84e2
David Elliott <elliott2081@gmail.com>
2014-09-09 12:47:13 -0500

    put social media buttons into seperate php file

--- ballot.php
+++ ballot.php
@@ -195,42 +195,13 @@ if($ballot_item_form->is_cancelled()) {
         $voter->mark_as_voted($election);
         
         echo $OUTPUT->header();
-        ?>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=715650648506222&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-    <?php
         echo $renderer->get_debug_info($voter->candoanything, $voter, $election);
         echo html_writer::tag('p', get_string('thanks_for_voting', 'block_sgelection'));
         echo html_writer::link($CFG->wwwroot, get_string('continue'));
-        // $result = $DB->get_records_sql('SELECT * FROM {table} WHERE foo = ?', array('bar'));
         $numberOfVotesTotal = $DB->count_records('block_sgelection_voted', array('election_id'=>$election->id));
         echo html_writer::tag('p', 'Number of votes cast so far ' . $numberOfVotesTotal);
-
-//            <a href="https://www.facebook.com/sharer/sharer.php?u=http://delliott.lsu.edu/mdl27/blocks/sgelection/ballot.php?election_id=1" target="_blank">
-//      Share on Facebook
-//    </a>
-        
-?>
-<div class = "socialmediabuttons">
-    <div class ="facingbooksharebutton">
-        <div class="fb-share-button" data-href="http://delliott.lsu.edu/mdl27/"></div>    
-    </div>
-    <div class="twitterybutton">
-        <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://delliott.lsu.edu/" data-lang="en" data-text="I just pretended to vote in the LSU Student Government Elections!">Tweet</a>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-    </div> 
-</div>
-
-<a href="http://www.tumblr.com/share/link?url=<?php echo urlencode('http://delliott.lsu.edu/mdl27/') ?>&name=<?php echo urlencode('LSU Student Government Election') ?>&description=<?php echo urlencode('LSU Student Government Elections') ?>" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:81px; height:20px; background:url('http://platform.tumblr.com/v1/share_1.png') top left no-repeat transparent;">Share on Tumblr</a>
-    <?php
-        
-    echo $OUTPUT->footer();
+        require_once 'socialmediabuttons.php';
+        echo $OUTPUT->footer();
 
     }
 
