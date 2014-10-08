52c6ae7bed35313ab8b13fe5576d4c832ae1f776
Robert Russo <rrusso@lsu.edu>
2014-10-08 14:27:51 -0500

    Updated facebook link for new image

--- fb.php
+++ fb.php
@@ -20,7 +20,7 @@ and open the template in the editor.
     <meta property="fb:app_id" content="715650648506222" /> 
     <meta property="og:type"   content="website" /> 
     <meta property="og:title"  content="I just voted on LSU's Student Government Elections!" /> 
-    <meta property="og:image"  content="<?php require_once('../../config.php'); global $CFG; echo $CFG->wwwroot ?>/theme/lsu/pix/vote_tigers_600.png" />
+    <meta property="og:image"  content="<?php require_once('../../config.php'); global $CFG; echo $CFG->wwwroot ?>/theme/lsu/pix/vote_tigers_fit.png" />
     <meta property="og:url"    content="<?php echo $CFG->wwwroot ?>/blocks/sgelection/fb.php" />
     <meta http-equiv="refresh" content="2;url=http://sg.lsu.edu/elections" />
     </head>
