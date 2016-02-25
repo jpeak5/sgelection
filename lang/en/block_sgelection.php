11bf50ee3a04541b50a0aa7b2bd1198e835bfae1
Chad Mazilly <chadmazilly@gmail.com>
2016-02-25 08:54:02 -0600

    change display of curriculum codes from multiple select to checkboxes

--- lang/en/block_sgelection.php
+++ lang/en/block_sgelection.php
@@ -83,6 +83,7 @@ $string['results_interval'] = "Results Email Interval";
 $string['results_interval_help'] = "Specify an interval (in minutes) that must elapse between email summaries";;
 $string['election_tool_administration'] = ' Election Tool Administration';
 $string['excluded_curriculum_code'] = 'Excluded Curriculum Code';
+$string['excluded_curriculum_codes'] = 'Excluded Curriculum Codes';
 
 // Commissioner Page / building election object
 $string['start_date'] = 'Start Date';
